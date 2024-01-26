610a8183792572696d1a0832f9ff7d03c228246b
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-01-26 16:46:10 +0530

    commit

---
+++ Data types/int.py
@@ -0,0 +1,23 @@
+a=57
+b=9963
+c=a+b
+ 
+print('Addition of two numbers is',c)
+
+
+
+
+d=b-a
+print('Subtraction of two numbers is',d)
+
+
+a=47477
+b=587864
+m=a*b
+print('Multiplication of two numbers is',m)
+
+s=b/a
+print('Division of two numbers is', s)
+
+
+

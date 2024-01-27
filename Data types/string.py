207d44c29c7706da17eb0c316d4b87be14e0d682
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-01-27 06:47:35 +0530

    commit1

--- Data types/string.py
+++ Data types/string.py
@@ -23,5 +23,3 @@ print(a[5])
 print(a[0])
 
 print(a*2 )
-
-print('yuvraj')
--- Data types/text_problem.py
+++ Data types/text_problem.py
@@ -1,1 +1,6 @@
-print('this file is for text data')
+#Problem1: if given n=3 then print =123
+
+n=3
+
+for i in range(1,n+1):
+    print(i,end='')

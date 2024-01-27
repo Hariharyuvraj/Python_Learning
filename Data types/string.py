57b6b8716c556086d485a072ad9116d7bce09495
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-01-27 17:31:16 +0530

    commit 2

--- Data types/string.py
+++ Data types/string.py
@@ -9,17 +9,28 @@ s2='maharastra'
 print (s2)
 s3='hindustan'
 
-a=s2+s3
+a=s2+s3     #we can add two str by using '+' operater.
 print(a)
 
 
 len(a)
 
 print(a[-1])
-
 print(a[-4])
 print(a[5])
 
 print(a[0])
 
 print(a*2 )
+
+# slice operater is apllicable in string data type,
+# we can collect necessory part of str by using slice operater.
+
+s1=a[1:3]
+print(s1)
+s2=a[0:7]
+print(s2)
+s3=a[:]
+print(s3)
+s4=a[0:-3]
+print(s4)

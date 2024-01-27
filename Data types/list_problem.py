47f3ebe3fb2f3c063f6c382dff230fa8620a8d7a
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-01-27 17:55:26 +0530

    list update

--- Data types/list_problem.py
+++ Data types/list_problem.py
@@ -1,11 +1,19 @@
-l=['1','2']
-print(l) 
+l=[10,20,30,'yuvraj','anuja','pune','tushar']
+print(l)
 
-list=['hrry','yuvraj']
+#:In list data type we can add any values like int,str,etc.
 
-l1=list[1]
+#: Append() 
+#: by using append we can add value in list 
+l.append(230)
+print(l)
+l.append('bunty')
+print(l)
 
-print(l1)
+# remove: by using this operater we can remove item from the list
 
-#note= this is list file
+l.remove(230)
+print(l)
 
+l.remove('bunty')
+print(l)

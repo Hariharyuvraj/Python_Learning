fa078e21c4731a018a6d2dd62a1c55cf4dacfb94
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-01-29 17:53:17 +0530

    updated

---
+++ Loops/fo_loop.py
@@ -0,0 +1,3 @@
+r=range(1,20,2)
+for x in r:
+    print(x)
--- Loops/if_else.py
+++ Loops/if_else.py
@@ -18,3 +18,34 @@ elif n%2 == 0 and n>=20:
         print(" this is fouth :not Weird")
 
         #: List is mutable ,we can change init
+
+# prob: 2
+        username=input('sagar')
+        password=input ('shiv')
+
+        if username =='sagar' and password =='shiv':
+               print("valid user")
+        else:
+               print("not valid user")
+
+
+a=3
+b=5
+if a>b:
+       print('yes')
+else:
+       print('no')
+
+a=50
+b=67
+if a<b:
+       print("barobar")
+else:
+       print('chuk')
+
+a=True
+b=False
+if a>b:
+       print('y')
+else:
+       print('n')

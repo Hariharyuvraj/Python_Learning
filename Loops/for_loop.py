d2999b99dd966686ddc887e4833a23e9c4ac4d83
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-02-08 15:11:15 +0530

    commit new

--- Loops/for_loop.py
+++ Loops/for_loop.py
@@ -1,3 +1,24 @@
 r=range(1,20,2)
 for x in r:
     print(x)
+
+# to print 10 times 
+    
+s='yuvraj Harihar'
+for x in s:
+    print(x)
+
+s1=range(10)
+for x in s1:
+    print('my name is yuvraj')
+
+# To display the odd number from 0 to 20
+    
+for x in range(21):
+ if (x%2!=0):
+    print(x)
+
+for x in range(21):
+ if (x%2):
+    print(x)
+
---
+++ Loops/operators.py
@@ -0,0 +1,35 @@
+# In python there are may operators available 
+# type 1: Arithmetic Oprators
+
++ ==  Addition
+- ==  Subtraction
+* ==  Multiplication
+/ ==  Division Operators
+% ==  Modulo Operators
+// == Floor division Operators
+** == Power division Operators
+
+
+# type 2: RelationalOperators
+
+>
+>=
+<
+<=
+
+
+# type 3: Equality Operators
+
+==  # equal to 
+!=  # not equal to
+
+# type 4: Assignment Operators
+
+
+# type 5: Special Operators
+# type 6: Logical Operators
+
+and
+or
+not
+

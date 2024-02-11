2f3b7e7dbecd457cb9b9e2eb0986f99005c9a8b4
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-02-11 14:10:54 +0530

    nh

---
+++ my practice.py
@@ -0,0 +1,69 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================================== RESTART: Shell =========================================
+print('hello')
+hello
+
+======================= RESTART: C:/Users/acs/Desktop/my practice.py ======================
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+hi hello,how r u
+my practice.py
+SyntaxError: invalid syntax
+r=range(8)
+for i in r:
+    print('hello yuvraj')
+
+    
+hello yuvraj
+hello yuvraj
+hello yuvraj
+hello yuvraj
+hello yuvraj
+hello yuvraj
+hello yuvraj
+hello yuvraj
+s='yuvraj i love u'
+for i in s:
+    print(i)
+
+    
+y
+u
+v
+r
+a
+j
+ 
+i
+ 
+l
+o
+v
+e
+ 
+u
+>>> u
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    u
+NameError: name 'u' is not defined
+>>> a=input('enter the number')
+enter the number474
+>>> b=input('enter num')
+enter num89384
+>>> if a>b:
+...     print('big number is',a)
+... else:
+...     print('enter onrt number')
+... 
+enter onrt number
+nkk

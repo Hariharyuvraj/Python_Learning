1dd47f4a4c8ce90f9b2b632e8104dfc0463e444c
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-02-11 07:50:01 +0530

    new added

---
+++ Loops/eval function.py
@@ -0,0 +1,11 @@
+## Eval funtion is useful to convert to variable type in to our input based value
+# As per our input changed this eval funtion show the data type of input value
+
+x=input('enter the any type of value:')
+print(type(x))
+
+# If i run this code then it will print type of value is "str"
+# But if i used the "eval"function then automatically shows type of entered value.
+
+x=eval(input('enter the any type of value:'))
+print(type(x))
--- Loops/while_loops.py
+++ Loops/while_loops.py
@@ -24,6 +24,16 @@ while i<=20:
 # To print the number which is divisible by 2 from 0 to 100
 i=1
 while i<=100:
-   if i%2==0:
-      print(i)
-      i=i+1
+    if i%2==0:
+       print(i)
+    i=i+1
+    
+ # the sum of n number:
+    
+n=int(input('enter the number:'))
+sum=0
+i=1
+while i<=10:
+   sum=sum+i
+   i=i+1
+print('the sum:',sum)

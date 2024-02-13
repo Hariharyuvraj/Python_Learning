4f3156c0064137eb0a5ef86767c95e878b2ecddb
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-02-13 14:04:14 +0530

    c1

---
+++ Loops/function.py
@@ -0,0 +1,20 @@
+def welcome(parameter):
+    '''doc str'''
+    body
+    return value
+
+# This is the formate of 'user define funtion'
+# Here 'return is not mandatory or optional.
+# In this eg parameter is mandatory,you have to give compulsory 'name' at the time of function calling.
+
+#e.g
+
+def msg():
+    print('welcome students in python learning program')
+
+#e.g
+
+def welcomemsg(name):
+    print('welcome students in python learning program specially',name)
+
+

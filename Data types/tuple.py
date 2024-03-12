16c9e274b86e2adc586cc20d8bebea2d40dc2ea4
Hariharyuvraj <hariharyuvraj99@gmail.com>
2024-03-12 14:19:52 +0530

    n

--- Data types/tuple.py
+++ Data types/tuple.py
@@ -13,3 +13,24 @@ s2=t[0:4]
 print(s2)
 
 # Note:in tuple single value ends with , then it shows type 'tuple'
+
+
+# function: 'reverse' the 'tuple'
+ 
+t=(1,2,3,4,5,6,7,8,9)
+r=reversed(t)
+t1=tuple(r)
+print(t1)
+
+# function:count()
+# by using this function we can count the specific number in the 'tuple'
+
+t=(1,2,3,1,1,1,4,5,6,7,8,9)
+print(t.count(1))
+
+# function: len()
+
+t=(2,1,2,3,1,1,1,4,5,6,7,8,9)
+print(len(t))
+
+print(t.index(1))      # using this we can find index of specific number in 'tuple'
